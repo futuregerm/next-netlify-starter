6e84f1aa69d944600cbb4eaae943ffe8fb4c700d
German <futuregerm@gmail.com>
2020-11-18 17:21:04 -0500

    Adding a page via next's Link and using the style jsx component.

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div>
+      Please contact me
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,69 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Link from "next/link";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Avocado!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the demo!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by clicking our contact page:{" "}
+          <Link href="/contact">Click here!</Link>
         </p>
       </main>
 
       <Footer />
+
+      <style jsx>
+        {`
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+          }
+
+          .container {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+        `}
+      </style>
+      <style jsx global>
+        {`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
